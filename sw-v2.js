183710ad9ce42b70c3d148cebbe6bf6fd3f3a8d9
Codely456 <theb12969@gmail.com>
2025-09-25 12:46:09 +0530

    Update sw-v2.js

--- sw-v2.js
+++ sw-v2.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'disaster-ready-v4';
+const CACHE_NAME = 'disaster-ready-v5';
 const FIREBASE_CACHE_NAME = 'firebase-cache-v1';
 const OFFLINE_URL = '/offline.html';
 const EMERGENCY_URL = '/emergency.html';
