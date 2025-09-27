03086c44fbd9ac96182649e516c1056baf1e648b
Codely456 <theb12969@gmail.com>
2025-09-28 00:06:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ let state = {
     appId: typeof __app_id !== 'undefined' ? __app_id : 'default-app-id',
     adminSchoolId: 'admin-school-123',
     userId: null,
-    userDisplayName: null,
     userRole: 'student',
     isAuthReady: false,
     admin: {
@@ -126,7 +125,6 @@ async function handleLogin(role) {
 
         state.isAuthReady = true;
         state.userId = user.uid;
-        state.userDisplayName = userId;
         state.userRole = role;
         localStorage.setItem('userRole', role); // Store role
         localDataService.setUserId(user.uid);
@@ -178,7 +176,7 @@ async function renderStudentDashboard() {
     localDataService.listenToUserData(state.appId, (docSnap) => {
         if (docSnap.exists()) {
             const data = docSnap.data();
-            renderStudentUI(state.userDisplayName, data, {
+            renderStudentUI(state.userId, data, {
                 onLogout: handleLogout,
                 onStartLearning: () => renderChapterList(data, chapterEventHandlers),
                 onTakeQuiz: (quizType) => showQuizDialog(quizType, (score) => handleQuizCompletion(quizType, score))
